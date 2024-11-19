a239bf9485fd3a49af90d1d046318b0003b341d5
Scott <sjb8108@rit.edu>
2024-11-19 02:38:26 -0500

    Messing with image recognition to make it so it only has to download osu std maps instead of downloading other modes and deleting the beatmap.

--- AutoDownloadAkatsukiDiscordMaps.py
+++ AutoDownloadAkatsukiDiscordMaps.py
@@ -2,29 +2,34 @@ import pyautogui
 import pydirectinput
 import time
 import pyperclip
+import numpy as np
+from skimage.metrics import structural_similarity as ssim
+import cv2
+
 #Any moveTo cords are specifically used for my montior resolution which is 2560 x 1440
 #FOR THE TIME OSU MUST HAVE THE SETTING FULL SCREEN MODE ON
 #Next step is to make it so it skips taiko, mania, and ctb by looking at the icon instead of downloading the map and such
 
+
 def main(mapNumber):
     downloadNumberOfMaps = mapNumber
     downloadedMaps = 0
+    iconStd = False
     while downloadedMaps < downloadNumberOfMaps:
-        pyautogui.PAUSE = 0.5
         pixelColor = pyautogui.pixel(770, 1200)
         if pixelColor == (43, 45, 49):
             pyautogui.moveTo(435, 1115)
         else:
             pyautogui.moveTo(430, 972)
+            iconStd = findIconWithBackGround()
         pyautogui.leftClick()
         downloadBeatmapTracker()
         pyautogui.hotkey('ctrl', 'alt', 'tab')
-        pyautogui.PAUSE = 2
+        time.sleep(2)
         if downloadedMaps == 0:
             pyautogui.press('right', presses=1)
         else:
             pyautogui.press('right')
-        pyautogui.PAUSE = 0.5
         pyautogui.moveTo(650,270)
         pyautogui.press('space')
         pyautogui.leftClick()
@@ -70,8 +75,23 @@ def getURL():
     pyautogui.leftClick()
     url = pyperclip.paste()
     return url
+
+def findIconWithBackGround():
+    return False
     
 if __name__ == "__main__": 
-    time.sleep(30)
-    main(54) #paramter set manually by user, have discord open, google tab open that isnt blank, osu with date added as caterogry and osu is muted
-    
+    image = pyautogui.screenshot(region=(399, 953, 20, 20))
+    image.save(r"C:\Users\Scott\Documents\OsuCoding\status.png")
+    imageIcon = np.array(image)
+    imageIconGrey = cv2.cvtColor(imageIcon, cv2.COLOR_BGR2GRAY)
+    IconStdImage = cv2.imread("IconStd.png")
+    IconStdImageGrey = cv2.cvtColor(IconStdImage, cv2.COLOR_BGR2GRAY)
+    differenceRankedFromStatus, _ = ssim(imageIconGrey, IconStdImageGrey, full=True)
+    print(differenceRankedFromStatus)
+    #time.sleep(30)
+    pyautogui.PAUSE = 0.5
+    #main(112) #paramter set manually by user, have discord open, google tab open that isnt blank, osu with date added as caterogry and osu is muted
+    #If the title and artist is 2 lines its region for icon is and have background (399, 953, 20, 20) Ex: Mero Meroido
+    #If three lines (399, 931, 20, 20) Ex: Geometertu Dash menu
+    #If one line (399, 975, 20, 20) Ex: My time
+    #if four line (super rare) (399, 909, 20, 20) Ex: Spiderman
