88243ae2510e6ba1609522b1ebaa8539938e9b55
Scott <sjb8108@rit.edu>
2025-02-12 22:51:57 -0500

    Made program for efficent and less buggy by getting rid of deleting beatmap from osu instead deletes from file folder

--- FixBeatmapOsuFiles.py
+++ FixBeatmapOsuFiles.py
@@ -4,9 +4,7 @@ import time
 import pyperclip
 
 #Any moveTo cords are specifically used for my montior resolution which is 2560 x 1440
-#FOR THE TIME OSU MUST HAVE THE SETTING FULL SCREEN MODE ON
-#Have it so tabs start with file explorer, then osu!, then google chrome from left to right
-#osu! must be in the group : "No Grouping" selected
+#Have it so tabs start with file explorer, then google chrome from left to right
 
 fileStopper = "2225895 "
 defaultBeatmapString = "https://osu.ppy.sh/beatmapsets/"
@@ -29,35 +27,21 @@ def main():
         fileBeatmap = fileBeatmap.split(" ")
         pyautogui.moveTo(15, 60)
         pyautogui.leftClick()
-        pyautogui.press('down')
         if fileBeatmap[0] == fileStopper:
             break
         elif len(fileBeatmap) > 1:
             pass
         else:
+            pyautogui.press('del')
+            pyautogui.press('down')
+            pyautogui.press('up')
             pyautogui.hotkey('ctrl', 'alt', 'tab')
-            if loopcounter > 0:
-                pyautogui.moveTo(1600, 500)
-                pyautogui.leftClick()
-            else:
-                pyautogui.moveTo(1250, 500)
-                pyautogui.leftClick()
+            pyautogui.press('enter')
             fixBeatmapFile(fileBeatmap[0])
         loopcounter+=1
         
         
 def fixBeatmapFile(beatmapID):
-    pyautogui.typewrite(beatmapID)
-    pydirectinput.moveTo(930, 1350)
-    pydirectinput.leftClick()
-    pydirectinput.moveTo(1500, 500)
-    pydirectinput.leftClick()
-    pydirectinput.moveTo(1500, 320)
-    pydirectinput.leftClick()
-    pydirectinput.press('backspace', presses=len(beatmapID))
-    pyautogui.hotkey('ctrl', 'alt', 'tab')
-    pyautogui.press('right')
-    pyautogui.press('enter')
     pyautogui.hotkey('ctrl', 't')
     beatmapURL = defaultBeatmapString + beatmapID
     pyautogui.typewrite(beatmapURL)
@@ -67,7 +51,6 @@ def fixBeatmapFile(beatmapID):
     time.sleep(5) #time to download beatmap
     pyautogui.hotkey('ctrl', 'w')
     pyautogui.hotkey('ctrl', 'alt', 'tab')
-    pyautogui.press('right')
     pyautogui.press('enter')
 
 if __name__ == "__main__":
