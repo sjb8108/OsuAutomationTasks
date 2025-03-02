94959aa9ee2ae4c7e3e9fa2cc4fc7cc5237aca9b
Scott <sjb8108@rit.edu>
2025-03-02 17:23:29 -0500

    Moved captured_status into images folder and minor changes to how beatmap is determined to be pending, and changed pause constant

--- FindAllPendingAndDeletedBeatmaps.py
+++ FindAllPendingAndDeletedBeatmaps.py
@@ -5,13 +5,12 @@ from skimage.metrics import structural_similarity as ssim
 import mss
 #Any moveTo cords are specifically used for my montior resolution which is 2560 x 1440
 #FOR THE TIME OSU MUST HAVE THE SETTING FULL SCREEN MODE OFF
-#Currently at keep weaving your spider way, spider way diff
+#Currently at Kano - Walk This Way! Mapper: Settia Diff: 1234
 #Fully Ran on Date: 12/24/2024
 #Should Run Next on Date: 04/01/2025
 #Goal: Combine MovePendingAndDeletedBeatmaps.py into here
 
 def main(numberOfMapsToProcess):
-    pydirectinput.PAUSE = 3
     pydirectinput.moveTo(1000, 600)
     rankedImage = cv2.imread("Images\Ranked.png")
     lovedImage = cv2.imread("Images\Loved.png")
@@ -29,7 +28,7 @@ def main(numberOfMapsToProcess):
         mapStatusImage = mss.mss().grab(region) 
         mapStatusIconImage = np.array(mapStatusImage)
         mapStatusImageGray = cv2.cvtColor(mapStatusIconImage, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("captured_status.png", mapStatusImageGray)
+        cv2.imwrite("Images\captured_status.png", mapStatusImageGray)
         differenceRankedFromStatus = ssim(mapStatusImageGray, rankedImageGray)
         differenceLovedFromStatus = ssim(mapStatusImageGray, lovedImageGray)
         differencePendingFromStatus = ssim(mapStatusImageGray, pendingImageGray)
@@ -41,9 +40,8 @@ def main(numberOfMapsToProcess):
         print(differenceRankedFromStatus)
         print(differenceRemovedFromStatus)
         
-        if differenceLovedFromStatus != 1 and differenceRankedFromStatus != 1:
+        if (differencePendingFromStatus > differenceLovedFromStatus and differencePendingFromStatus > differenceRankedFromStatus) or (differenceRemovedFromStatus > differenceLovedFromStatus and differenceRemovedFromStatus > differenceRankedFromStatus):
             
-            pydirectinput.PAUSE = 0.5
             pydirectinput.moveTo(1500, 700)
             pydirectinput.rightClick()
             pydirectinput.moveTo(1500, 400)
@@ -53,9 +51,9 @@ def main(numberOfMapsToProcess):
             pydirectinput.moveTo(1650, 1150)
             pydirectinput.leftClick()
             pydirectinput.moveTo(1600, 800)
-            pydirectinput.PAUSE = 3
                 
         pydirectinput.press("right")
         numberOfMapsToProcess-=1
 
-main(4000)
+pydirectinput.PAUSE = 1.5
+main(7400)
