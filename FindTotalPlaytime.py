e5260154194d9ebeececa6026ae4a4610c03880d
Scott <sjb8108@rit.edu>
2025-03-12 09:38:01 -0400

    FindTotalPlaytime.py finished

--- FindTotalPlaytime.py
+++ FindTotalPlaytime.py
@@ -1,7 +1,15 @@
 from osu import Client
+import osu
 import time
-from bs4 import BeautifulSoup
 import requests
+import math
+
+#Run every 6 months
+#Last Run: 3/11/2025
+#Next Run: 6/1/2025
+#Current Total osu! Playtime: 22 days (Bancho) + 33 days (Akatsuki recorded in most played beatmaps ~24k = 68%) + 16 days (Average for playtime that wasnt recorded ~11k = 31%) = ~71 days +-1 at 25%
+#Current Total osu! Playtime: 22 days (Bancho) + 41 days (Akatsuki recorded in most played beatmaps ~24k = 68%) + 19 days (Average for playtime that wasnt recorded ~11k = 31%) = ~82 days +-1 at 50% 
+
 
 client_id = 29661
 client_secret = "CwzqipNRs7axuZ0C43PwEFzpe4BD3g5tyyiVqlTA"
@@ -13,20 +21,44 @@ pageNumber = 1
 url = "https://akatsuki.gg/api/v1/users/most_played?id=81870&mode=0&p="+str(pageNumber)+"&l=100&rx=1"
 mapData = requests.get(url).json()
 while (mapData['most_played_beatmaps'] != None):
-    print(pageNumber)
     entireMapList = entireMapList + mapData['most_played_beatmaps']
     pageNumber+=1
     url = "https://akatsuki.gg/api/v1/users/most_played?id=81870&mode=0&p="+str(pageNumber)+"&l=100&rx=1"
     time.sleep(1)
     mapData = requests.get(url).json()
 
-approxPlayTime = 0
+print("Loading playcounts completed.")
+#Can make more variable if I wanted to have different distrubitions current is 50%
+approxPlayTimeSeconds = 0
+totalPlaycount = 0
 for index in range(0, len(entireMapList)):
-    print("Current Index: " + index)
     beatmap = entireMapList[index]
     beatmapID = beatmap['beatmap']['beatmap_id']
     playcount = beatmap['playcount']
-    osuBeatmap = client.get_beatmap(beatmapID)
+    try:
+        osuBeatmap = client.get_beatmap(beatmapID)
+    except(osu.exceptions.RequestException):
+        continue
     lengthOfMap = osuBeatmap.hit_length
-    #need to figure out a good distrubition
+    totalPlaycount += playcount
+    print("Current Index: " + str(index) + "(" + (str((len(entireMapList)) - index)) + " Remaining)")
+    print("Current Map: " + beatmap['beatmap']['song_name'])
+    print("Current Total Playcount: " + str(totalPlaycount))
+    if playcount == 1:
+        approxPlayTimeSeconds += lengthOfMap
+    else:
+        approxPlayTimeSeconds += lengthOfMap
+        playcount-=1
+        distributedLength = math.floor(lengthOfMap * 0.5)
+        restOfPlaytimeForMap = distributedLength * playcount
+        approxPlayTimeSeconds += restOfPlaytimeForMap
+    print("Current Playtime: " + str(approxPlayTimeSeconds))
     time.sleep(1)
+
+print("Approx Playtime in Seconds: " + str(approxPlayTimeSeconds))
+approxPlayTimeMinutes = approxPlayTimeSeconds / 60
+print("Approx Playtime in Minutes: " + str(approxPlayTimeMinutes))
+approxPlayTimeHours = approxPlayTimeMinutes / 60
+print("Approx Playtime in Hours: " + str(approxPlayTimeHours))
+approxPlayTimeDays = approxPlayTimeHours / 24
+print("Approx Playtime in Days: " + str(approxPlayTimeDays))
