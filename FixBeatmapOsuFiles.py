ad949861658003e33b7fbfe291084599409a6c60
Scott <sjb8108@rit.edu>
2025-04-25 00:53:09 -0400

    Another reprogramming of how to find file names in FixBeatmapOsuFiles.py ...

--- FixBeatmapOsuFiles.py
+++ FixBeatmapOsuFiles.py
@@ -1,7 +1,6 @@
 import pyautogui
 import time
 import pyperclip
-import cv2
 
 #Any moveTo cords are specifically used for my montior resolution which is 2560 x 1440
 #Have it so tabs start with file explorer, then google chrome from left to right, osu must still be open but the first two tabs are file and chrome
@@ -12,15 +11,19 @@ import cv2
 #Make it so it left clicks folder and gets id instead of having it open the file location as opening file location has caused problems if 
 #it takes to long to load in
 
-fileStopper = "1640260"
+fileStopper = "2239324"
 defaultBeatmapString = "https://osu.ppy.sh/beatmapsets/"
 
 def main():
     fileBeatmap = ""
+    y_cord = 120
     pyautogui.moveTo(550, 120)
     pyautogui.leftClick()
     while (True):
-        pyautogui.moveTo(600, 600)
+        if y_cord > 1360:
+            y_cord = 1365 #this will need to be changed
+        pyautogui.moveTo(550, y_cord)
+        pyautogui.leftClick()
         pyautogui.hotkey('alt', 'enter')
         loc = pyautogui.locateOnScreen("Images\\fileNameFinder.png")
         pyautogui.moveTo(loc[0]+20, loc[1]-30)
@@ -33,11 +36,9 @@ def main():
         if fileBeatmap[0] == fileStopper:
             break
         elif len(fileBeatmap) > 1:
-            pyautogui.press('down')
+            y_cord+=21
         else:
             pyautogui.press('del')
-            pyautogui.press('down')
-            pyautogui.press('up')
             pyautogui.hotkey('ctrl', 'alt', 'tab')
             pyautogui.press('enter')
             fixBeatmapFile(fileBeatmap[0])
@@ -48,7 +49,8 @@ def fixBeatmapFile(beatmapID):
     beatmapURL = defaultBeatmapString + beatmapID
     pyautogui.typewrite(beatmapURL)
     pyautogui.press('enter')
-    pyautogui.moveTo(900, 605)
+    #maybe want to wait another 0.5 seconds just for the url to load
+    pyautogui.moveTo(900, 605) #should have it find the download button instead of going to a cordinate of it
     pyautogui.leftClick()
     time.sleep(5) #time to download beatmap
     pyautogui.hotkey('ctrl', 'w')
@@ -56,6 +58,6 @@ def fixBeatmapFile(beatmapID):
     pyautogui.press('enter')
 
 if __name__ == "__main__":
-    time.sleep(3)
-    pyautogui.PAUSE = 1
+    time.sleep(30)
+    pyautogui.PAUSE = 0.5
     main()
