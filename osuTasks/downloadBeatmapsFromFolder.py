b53650d84baec03e0a435accc91839aa59db4c70
Scott <sjb8108@rit.edu>
2025-11-09 03:27:01 -0500

    bug fix if beatmap was a taiko/catch/mania map and added logic for using cv instead of pixel values

--- osuTasks/downloadBeatmapsFromFolder.py
+++ osuTasks/downloadBeatmapsFromFolder.py
@@ -12,28 +12,39 @@ def main():
         for map in myfile:
             allBeatmaps.append(map)
     currentIndex = 1
+    resume = "221870"
     for beatmap in allBeatmaps:
         print("Downloading " + str(beatmap))
         print("Currently at index " + str(currentIndex) + " / " + str(len(allBeatmaps)))
         beatmapID = beatmap.split(".")[0]
+        if int(resume) > int(beatmapID):
+            currentIndex += 1
+            continue
         pyautogui.moveTo(900,300)
         pyautogui.leftClick()
         pyautogui.write(beatmapID)
         pyautogui.press('enter')
         pageLoad = 0
         pageError = True
+        nonStdBeatmap = False
         while pageLoad != 120:
             try:
-                pageComplete = pyautogui.locateCenterOnScreen("Images\\IconStd.png", confidence=0.70)
+                pageComplete = pyautogui.locateCenterOnScreen("Images\\banchoIconStd.png", confidence=0.85)
                 pageError = False
                 break
             except:
-                pageLoad+=1
+                try:
+                    pageComplete = pyautogui.locateCenterOnScreen("Images\\banchoBeatmapError.png", confidence=0.95)
+                    nonStdBeatmap = True
+                except:
+                    pageLoad+=1
             time.sleep(.25)
-        pyautogui.moveTo(1250, 615)
-        pyautogui.leftClick()
+        if nonStdBeatmap:
+            continue
         if pageError:
             raise Exception("Osu Beatmap Page Did Not Load")
+        pyautogui.moveTo(1250, 615)
+        pyautogui.leftClick()
         downloadCounter = 0
         downloadError = True
         while downloadCounter != 120:
@@ -56,6 +67,6 @@ def main():
         currentIndex+=1
         
 if __name__ == "__main__":
-    time.sleep(10)
+    #time.sleep(10)
     pyautogui.PAUSE = 0.5
     main()
