b36a392fce947306730401f9cf55ba98f4f5c141
Scott <sjb8108@rit.edu>
2025-12-11 20:31:35 -0500

    findTotalTimeLeftInCollection.py finished

--- osuTasks/findTotalTimeLeftInCollection.py
+++ osuTasks/findTotalTimeLeftInCollection.py
@@ -1,16 +1,34 @@
 import pytesseract
 import pyautogui
 import time
+import pydirectinput
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 def main(numOfMaps) -> None:
     mapsDone = 0
+    totalTimeInSeconds = 0
     while(mapsDone != numOfMaps):
-        currentTime = pyautogui.screenshot("Images\\beatmapLength.png", region=(120, 75, 75, 35))
+        currentTime = pyautogui.screenshot("Images\\beatmapLength.png", region=(120, 75, 83, 35))
         text = pytesseract.image_to_string(currentTime)
-        print(text)
+        minute = int(text[0:2])
+        seconds = int(text[3:5])
+        totalTimeInSeconds = totalTimeInSeconds + ((minute * 60) + seconds)
         mapsDone+=1
+        print("Maps Done: " + str(mapsDone) + " / " + str(numOfMaps))
+        nextBeatmapPixel = pyautogui.pixel(2300,800)
+        redValue = nextBeatmapPixel[0]
+        if redValue <= 10:
+            pydirectinput.press('down')
+        else:
+            pydirectinput.press('right')
+        time.sleep(.5)
         
+    print("Approx Completion of Collection in Seconds: " + str(totalTimeInSeconds))
+    totalTimeInMinutes = totalTimeInSeconds / 60
+    print("Approx Completion of Collection in Minutes: " + str(totalTimeInMinutes))
+    totalTimeInHours = totalTimeInMinutes / 60
+    print("Approx Completion of Collection in Hours: " + str(totalTimeInHours))
 if __name__ == "__main__":
-    main(1)
+    time.sleep(3)
+    main(7)
